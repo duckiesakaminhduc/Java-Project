88690929036ea3faac35d1a6baf1506fdf0f161a
duckiesakaminhduc <voducminh39@gmail.com>
2022-11-26 09:49:47 +0700

    chinh sua lan 2

--- Test1/src/test1.java
+++ Test1/src/test1.java
@@ -3,5 +3,6 @@ public class test1 {
 public static void main(String[] args) {
 	System.out.println("hello");
 	System.out.println("tau met lam roi");
+	System.out.println("dan choi so 1 dong nai");
 }
 }
