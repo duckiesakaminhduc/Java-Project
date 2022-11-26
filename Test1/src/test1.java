900b20d3925ed00ba89a79a8c55fca65cdff95a9
duckiesakaminhduc <voducminh39@gmail.com>
2022-11-26 09:53:43 +0700

    sua lan 4

--- Test1/src/test1.java
+++ Test1/src/test1.java
@@ -4,5 +4,6 @@ public static void main(String[] args) {
 	System.out.println("hello");
 	System.out.println("tau met lam roi");
 	System.out.println("dan choi so 1 dong nai");
+	System.out.println("met");
 }
 }
